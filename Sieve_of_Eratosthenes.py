2f13456cb607cba8dbeb4265b8fc5c3b821b38c0
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-03-11 17:49:05 +0000

    Add files via upload

---
+++ Sieve_of_Eratosthenes.py
@@ -0,0 +1,17 @@
+def SOE(num):
+    prime = [True for i in range(num+1)]
+
+    p = 2
+    while (p ** 2 <= num):
+
+        if (prime[p] == True):
+
+            for i in range(p ** 2, num+1, p):
+                prime[i] = False
+        p += 1
+    
+    return [p for p in range(2, num+1) if prime[p]]
+
+
+if __name__ == "__main__":
+    print(SOE(10))
