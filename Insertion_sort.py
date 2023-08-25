3b893db2a44cc04d7e5b01ec99ff201212b1a2bc
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-08-25 20:39:18 +0100

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -2,21 +2,30 @@ def insertionSort(arr):
      
     if (n := len(arr)) <= 1:
       return
+    
     for i in range(1, n):
-         
         key = arr[i]
- 
-        # Move elements of arr[0..i-1], that are
-        # greater than key, to one position ahead
-        # of their current position
         j = i-1
-        while j >=0 and key < arr[j] :
-                arr[j+1] = arr[j]
-                j -= 1
+    
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
         arr[j+1] = key
+
+def insertionSortRecusive(arr, n):
+    if n <= 1:
+        return
+    
+    insertionSortRecusive(arr, n-1)
+    last = arr[n-1]
+    j = n-2
+
+    while j >= 0 and arr[j] > last:
+        arr[j + 1] = arr[j]
+        j -= 1
+    arr[j+1] = last
  
- 
-#sorting the array [12, 11, 13, 5, 6] using insertionSort
-arr = [12, 11, 13, 5, 6]
+
+arr = [4, 3, 5, 0, 1]
 insertionSort(arr)
 print(arr)
